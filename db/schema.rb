74533e318d9897c36d84fce41bf9b56b68ffe2b4
Noah <noahramey1997@gmail.com>
2017-07-26 19:52:39 -0700

    add seed file, add name to accounts, style vault page, realize i made a huge mistake using bcrypt, will probably be getting rid of that asap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711024313) do
+ActiveRecord::Schema.define(version: 20170727023724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170711024313) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# SEED FILE
+
+# Test Users
+@user1 = User.create(name: "User 1", email: "user1@test.com", password: "test1234")
+@user2 = User.create(name: "User 2", email: "user2@test.com", password: "test1234")
